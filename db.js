a79c0b4d2bf777e1a1d8ae9231a9830aea2ababf
Christo-lk <73858996+Christo-lk@users.noreply.github.com>
2021-01-22 10:49:54 +1300

    listAllBooks works

--- db.js
+++ db.js
@@ -1,1 +1,13 @@
-module.exports = {}
+// const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile').development
+const database = require('knex')(config)
+
+
+module.exports = {
+    listAllBooks: listAllBooks
+}
+
+function listAllBooks(db =database){
+
+  return db('books')
+}
