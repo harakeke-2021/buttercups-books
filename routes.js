f45ca0e6cc8efbfd1f212a9416914794686290a5
wallace222 <54966219+wallace222@users.noreply.github.com>
2021-01-22 10:53:28 +1300

    create root route

--- routes.js
+++ routes.js
@@ -3,9 +3,19 @@ const db = require('./db')
 
 const router = express.Router()
 
-
+//get all books
+//render to home
 router.get('/', (req, res) => {
-  res.send('homepage!')
+  db.listAllBooks()
+  .then(books => {
+    const viewData = {
+      bookList: books,
+      currentUser: {}
+    }
+    res.render('home', viewData)
+  })
 })
+
+router.get('')
 
 module.exports = router
