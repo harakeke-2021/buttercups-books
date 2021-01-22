8862463d39e631e5328f29cd054e998f564684b4
wallace222 <54966219+wallace222@users.noreply.github.com>
2021-01-22 14:51:49 +1300

    add donate routes

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const db = require('./db')
 
 const router = express.Router()
 
+// HOMEPAGE
 router.get('/', (req, res) => {
   console.log('cookies:', req.cookies)
   db.listAllBooks()
@@ -14,6 +15,7 @@ router.get('/', (req, res) => {
     })
 })
 
+// FUNCTION TESTING ROUTE
 router.get('/functions/:id', (req, res) => {
 
   const id = Number(req.params.id)
@@ -26,13 +28,45 @@ router.get('/functions/:id', (req, res) => {
   res.send(`functions testing! ${id}`)
 })
 
+// LOGIN GET ROUTE
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// LOGIN POST ROUTE
  router.post('/login', (req, res) => {
   console.log(req.body)
 })
+
+//PLEASE REMOVE LATER  .PLACEHOLDER id is meant to represent for cookie user id. PLEASE REMOVE LATER
+router.get('/donate/:id', (req,res) => {
+  const id = 10//req.params.id
+  
+  viewData = {
+    id: id
+  }
+  res.render('donate', viewData)
+})
+
+// DONATE POST ROUTE
+router.post('/donate/:id', (req,res) => {
+
+  const {title, author, pubDate} = req.body
+
+  const viewData = {
+    donor_id: 1,
+    title: title,
+    author:author,
+    pub_date: pubDate
+  }
+
+  db.donateBook(viewData)
+    .then((result) => {
+      res.redirect('/')
+    })
+
+})
+
 
 
 module.exports = router
